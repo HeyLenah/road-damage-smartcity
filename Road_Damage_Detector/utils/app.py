adc6cd35344f36d43a2129742f4d61ffbf3c0139
Deema Alluhayb <deemaal.lhaib@gmail.com>
2025-04-30 22:04:31 +0300

    Test6

--- Road_Damage_Detector/utils/app.py
+++ Road_Damage_Detector/utils/app.py
@@ -14,7 +14,7 @@ USER_DB_FILE = "users.json"
 HISTORY_FILE = "history.pkl"
 
 # API URL
-API_URL= 'https://image-thabat-652749443637.europe-west1.run.app'
+API_URL= 'https://image-thabat-652749443637.europe-west1.run.app/predict'
 
 # Load and Save functions
 def load_user_db():
